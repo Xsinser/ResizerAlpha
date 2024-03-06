c55d16abe392085caa43ba2eee3ebbf6b80f1a37
Xsinser <Xsinser@DESKTOP-ODPJPLC>
2024-03-06 23:59:48 +0300

    P1

---
+++ XResize.Core/Models/Processor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.ML.OnnxRuntime;
+using SkiaSharp;
+
+namespace XResize.Models
+{
+    public abstract class Processor
+    {
+        public abstract Task<SKBitmap?> Resize(SKBitmap image);
+
+        protected abstract void ImageToSmallImages(int numberElementByHeight, int numberElementByWidth,
+                                                            SKBitmap image, out SKBitmap[][] bitmaps);
+
+        protected abstract SKBitmap ImagesToFullImage(in SKBitmap[][] bitmaps, int width, int height);
+
+        protected abstract SKBitmap Resizer(SKBitmap bitmap, InferenceSession session);
+
+        protected abstract SKBitmap SaveAsFile(float[] bufer);
+    }
+}
---
+++ XResizer.Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using ImResizer.Models.ESRGANSR4;
+using SkiaSharp;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace XResizer.Test
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            ILoggerFactory factory = LoggerFactory.Create(builder => builder.AddConsole());
+            ILogger logger = factory.CreateLogger("Program");
+            var processor = new EsrganSRFourProcessor(
+                Path.Combine(Directory.GetCurrentDirectory(), Path.Combine("OnnxModels", "ESRGAN_SRx4_DF2KOST_official-ff704c303320.onnx")),//// Путь до обученной либы нейронки
+                1,//// Количество паралельных потоков для увеличения разрешения в рамках одного Resize
+                logger);/// Logger
+
+            var originalImage =  SKBitmap.Decode(("old.jpg"));
+            var resultImage = processor.Resize(originalImage).Result;
+
+            using (var data = resultImage.Encode(SKEncodedImageFormat.Jpeg, 100))
+            using (var stream = File.OpenWrite("new.jpg"))
+            {
+                data.SaveTo(stream);
+            }
+        }
+    }
+}
